ec1a20d144eb656ba3b27731a15147522cf60e29
Callum Gilfedder <Callumgilf@mail.com>
2022-11-15 14:08:35 +0000

    Finished version 1 as of now

--- script.js
+++ script.js
@@ -3,33 +3,75 @@ var answer_button = document.getElementById("answer");
 answer_button.innerHTML = "";
 
 var number_buttons = document.getElementsByClassName("item1");
-console.log(number_buttons)
 
+var operator = "default";
+var savedValue = 0;
 // loop through and add event listener to each one, each time a button is clicked it reaches for the inner html and 
 // appends that value to the answer_button.
 operatorsList = ["answer", "+", "x", "-", "AC", "="] 
 
-if (operatorsList.includes("+")) {
-}
+
 
 // need to make sure that . cant be added more than once or at the beginning.
 
 function getInnerHTML() {
-    console.log(this.innerHTML)
     if (this.innerHTML === ".") {
-        if (answer_button.innerHTML.includes(".")) {
+        if (answer_button.innerHTML.includes(".") || answer_button.innerHTML == "") {
             console.log("Already added .")
         } else {
             answer_button.innerHTML = answer_button.innerHTML + this.innerHTML
         }
     }
     if (operatorsList.includes(this.innerHTML)) {
-        console.log("Not working yet")
+        if (this.innerHTML==="+") {
+            savedValue = answer_button.innerHTML
+            answer_button.innerHTML = "";
+            operator = "+";
+            // console.log(operator)
+        }
+        if (this.innerHTML==="x") {
+            savedValue = answer_button.innerHTML
+            answer_button.innerHTML = "";
+            operator = "x";
+
+        }
+        if (this.innerHTML==="-") {
+            savedValue = answer_button.innerHTML
+            answer_button.innerHTML = "";
+            operator = "-";
+        }
+        if (this.innerHTML==="AC") {
+            answer_button.innerHTML = "";
+        }
+        if (this.innerHTML === "=") {
+            if (operator === "+") {
+                finalValue = Number(answer_button.innerHTML) + Number(savedValue);
+                answer_button.innerHTML = (finalValue);
+            }
+            if (operator === "-") {
+                finalValue = Number(answer_button.innerHTML) - Number(savedValue);
+                answer_button.innerHTML = finalValue;
+
+            }
+            if (operator === "x") {
+                finalValue = Number(answer_button.innerHTML) * Number(savedValue);
+                answer_button.innerHTML = finalValue;
+
+            }
+            
+        }
     } else if (this.innerHTML != ".") {
         answer_button.innerHTML = answer_button.innerHTML + this.innerHTML
-
     }
+    
+
+
+
+
+
 }
+
+
 
 var i = 0;
 while (i < number_buttons.length) {
